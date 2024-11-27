3f716fe0c108872194311a917686878ae59c688a
milanychh <kudashevamilana@mail.ru>
2024-11-27 20:46:37 +0400

    Добавлены эндпоинты для получения всех клиентов

--- src/project/api/routes.py
+++ src/project/api/routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from project.infrastructure.postgres.repository.user_repo import UserRepository
+from project.infrastructure.postgres.repository.client_repo import ClientRepository
 from project.infrastructure.postgres.database import PostgresDatabase
 from project.schemas.user import UserSchema
+from project.schemas.client import ClientSchema
 
 
 router = APIRouter()
@@ -18,3 +20,14 @@ async def get_all_users() -> list[UserSchema]:
         all_users = await user_repo.get_all_users(session=session)
 
     return all_users
+
+@router.get("/all_clients", response_model=list[ClientSchema])
+async def get_all_clients() -> list[ClientSchema]:
+    client_repo = ClientRepository()
+    database = PostgresDatabase()
+
+    async with database.session() as session:
+        await client_repo.check_connection(session=session)
+        all_clients = await client_repo.get_all_clients(session=session)
+
+    return all_clients
