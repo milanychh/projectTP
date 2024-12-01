32dcd157eb88f2e3895c43c64d79a193ad9c91be
milanychh <kudashevamilana@mail.ru>
2024-12-01 21:50:04 +0400

    Добавлены SQLAlchemy-модели: User, Client, Product, Supplier, Delivery, DeliveryItem

--- src/project/infrastructure/postgres/models.py
+++ src/project/infrastructure/postgres/models.py
@@ -1,4 +1,7 @@
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
+from decimal import Decimal
 
 from project.infrastructure.postgres.database import Base
 
@@ -21,3 +24,33 @@ class Client(Base):
     full_name: Mapped[str] = mapped_column(nullable=False)
     phone_number: Mapped[str] = mapped_column(nullable=True)
     email: Mapped[str] = mapped_column(nullable=True)
+
+class Product(Base):
+    __tablename__ = "products"
+
+    product_id: Mapped[int] = mapped_column(primary_key=True)
+    product_name: Mapped[str] = mapped_column(nullable=False)
+    expiry_date: Mapped[str | None] = mapped_column(nullable=True)
+
+class Supplier(Base):
+    __tablename__ = "suppliers"
+
+    supplier_id: Mapped[int] = mapped_column(primary_key=True)
+    supplier_name: Mapped[str] = mapped_column(nullable=False)
+    contact_info: Mapped[str] = mapped_column(nullable=True)
+
+class Delivery(Base):
+    __tablename__ = "deliveries"
+
+    delivery_id: Mapped[int] = mapped_column(primary_key=True)
+    supplier_id: Mapped[int] = mapped_column(ForeignKey("suppliers.supplier_id", ondelete="CASCADE"), nullable=False)
+    delivery_date: Mapped[date] = mapped_column(nullable=True)
+
+class DeliveryItem(Base):
+    __tablename__ = "delivery_items"
+
+    item_id: Mapped[int] = mapped_column(primary_key=True)
+    delivery_id: Mapped[int] = mapped_column(ForeignKey("deliveries.delivery_id", ondelete="CASCADE"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.product_id", ondelete="CASCADE"), nullable=False)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+    unit_price: Mapped[Decimal] = mapped_column(nullable=False)
